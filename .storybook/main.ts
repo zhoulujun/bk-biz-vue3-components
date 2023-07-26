e51c866a1531f473b8927cd9c62db8e399e247b7
andyljzhou <1@zhoulujun.cn>
2023-07-26 15:16:00 +0800

    docs: 人员选择器文档说明改进

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,28 +1,26 @@
 import type { StorybookConfig } from '@storybook/vue3-vite';
-
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
-    '@storybook/addon-docs',
-    {
-      name: '@storybook/addon-styling',
-      options: {
-        sass: {
-          // Require your Sass preprocessor here
-          implementation: require('sass'),
-        },
-      },
-    },
-  ],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions', {
+    name: '@storybook/addon-docs',
+    options: {
+      configureJSX: true
+    }
+  }, {
+    name: '@storybook/addon-styling',
+    options: {
+      sass: {
+        // Require your Sass preprocessor here
+        implementation: require('sass')
+      }
+    }
+  }, '@storybook/addon-mdx-gfm'],
   framework: {
     name: '@storybook/vue3-vite',
-    options: {},
+    options: {}
   },
   docs: {
-    autodocs: 'tag',
-  },
+    autodocs: 'tag'
+  }
 };
 export default config;
